413e1f519ac43e965a0a64f1c92a26faab96d28a
DESKTOP-ELLOS\Usuário <giovanijm@gmail.com>
2023-05-02 00:30:03 -0300

    Campo Imagem Usuário

--- resources/views/admin/manager-user/users/_partials/form.blade.php
+++ resources/views/admin/manager-user/users/_partials/form.blade.php
@@ -144,6 +144,23 @@
             </div>
         </div>
     </div>
+    @if($pageOrigem == 'edit')
+        <div class="grid grid-cols-1 sm:grid-cols-3 sm:gap-4 py-2">
+            <div class="col-span-3">
+                <x-input-label for="created_at" class="block mb-1 text-sm font-bold text-gray-700" :value="__('Foto do Usuário').':'"/>
+                <div class="flex items-center justify-center w-full">
+                    <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                        <div class="flex flex-col items-center justify-center pt-5 pb-6">
+                            <svg aria-hidden="true" class="w-10 h-10 mb-3 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
+                            <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Clique para fazer o upload</span> ou arraste o arquivo.</p>
+                            <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+                        </div>
+                        <input id="dropzone-file" type="file" class="hidden" />
+                    </label>
+                </div>
+            </div>
+        </div>
+    @endif
     <div class="grid grid-cols-1 sm:grid-cols-2 sm:gap-4">
         <div class="col-span-2 sm:col-span-1 pb-2 sm:pb-0">
             <x-input-label for="created_at" class="block mb-1 text-sm font-bold text-gray-700" :value="__('Data de Criação').':'"/>
