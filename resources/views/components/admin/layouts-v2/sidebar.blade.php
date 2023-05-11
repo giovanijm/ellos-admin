0a954db55cc3f9a08dc45e24bdef07f4ff9ce377
DESKTOP-ELLOS\Usuário <giovanijm@gmail.com>
2023-05-11 02:36:01 -0300

    Aplicação de permissão exibir menus

--- resources/views/components/admin/layouts-v2/sidebar.blade.php
+++ resources/views/components/admin/layouts-v2/sidebar.blade.php
@@ -1,3 +1,8 @@
+@php
+    use \App\Models\Admin\ManagerUser\Permission;
+
+    $moduleSettings = new Permission(); $moduleSettings->name = "moduleSettings";
+@endphp
 <div id="application-sidebar"
     class="hs-overlay hs-overlay-open:translate-x-0 -translate-x-full transition-all duration-300 transform scrollbar-y dark:scrollbar-y z-[60] hidden xl:block fixed top-0 left-0 bottom-0 overflow-y-auto xl:translate-x-0 xl:right-auto xl:bottom-0 ellos-sidebar"
 >
@@ -13,14 +18,16 @@
             <x-eos-home class="icon-item" />
             <span class="ml-2">{{ __('admin/sidebar.labelHome') }}</span>
         </x-admin.layouts-v2.nav-modulo-item>
-        <x-admin.layouts-v2.nav-modulo-item
-            href="{{ route('admin.manager-user') }}"
-            class="{{ request()->routeIs('admin.users.*') || request()->routeIs('admin.roles.*') || request()->routeIs('admin.permissions.*') ? 'modulo-item-active' : 'modulo-item' }}"
-            selected="{{ request()->routeIs('admin.users.*') || request()->routeIs('admin.roles.*') || request()->routeIs('admin.permissions.*') ? 'item-selected' : '' }}"
-        >
-            <x-eos-settings class="icon-item" />
-            <span class="ml-2">{{ __('Configurações') }}</span>
-        </x-admin.layouts-v2.nav-modulo-item>
+        @can('view', $moduleSettings)
+            <x-admin.layouts-v2.nav-modulo-item
+                href="{{ route('admin.manager-user') }}"
+                class="{{ request()->routeIs('admin.users.*') || request()->routeIs('admin.roles.*') || request()->routeIs('admin.permissions.*') ? 'modulo-item-active' : 'modulo-item' }}"
+                selected="{{ request()->routeIs('admin.users.*') || request()->routeIs('admin.roles.*') || request()->routeIs('admin.permissions.*') ? 'item-selected' : '' }}"
+            >
+                <x-eos-settings class="icon-item" />
+                <span class="ml-2">{{ __('Configurações') }}</span>
+            </x-admin.layouts-v2.nav-modulo-item>
+        @endcan
     </x-admin.layouts-v2.nav-modulo>
 
     @yield('submenu')
